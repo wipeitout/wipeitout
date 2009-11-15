e576b7b593171694ee531bedb84354248fd1bc2c
gm <gm@gm-laptop.(none)>
2009-11-15 20:00:50 +1100

    fixed styling

---
+++ app/models/supermarket_mailer.rb
@@ -0,0 +1,13 @@
+class SupermarketMailer < ActionMailer::Base
+  
+
+  def pledge(supporter, supermarket)
+    subject    'Please help wipe out non-recycled toilet paper'
+    recipients supermarket.email_to_address
+    from       supporter.email
+    sent_on    Time.now
+    
+    body       :supporter => supporter, :supermarket => supermarket
+  end
+
+end
