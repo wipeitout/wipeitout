a2bb27be8a613b803db29882a6fc229e98b870d7
gm <gm@gm-laptop.(none)>
2009-11-15 18:49:36 +1100

    removed username and password from smtp_gmail file

--- config/initializers/smtp_gmail.rb
+++ config/initializers/smtp_gmail.rb
@@ -1,22 +1,11 @@
-#ActionMailer::Base.smtp_settings = {
-#        :address        => "smtp.gmail.com",
-#        :port           => 587,
-#        :authentication => :plain,
-#        :enable_starttls_auto => true,
-#        :user_name      => "aaron@wipeitout.com.au",
-#        :password       => "aaron77"
-#}
-
-
-#ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
         :enable_starttls_auto => true, 
         :address => "smtp.gmail.com",
         :domain => "wipeitout.com.au",
         :port => 587,
         :authentication => :plain,
-        :user_name => "aaron@wipeitout.com.au",
-        :password => "aaron77" }
+        :user_name => "xxx",
+        :password => "yyy" }
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.default_charset = "utf-8"
