fa5bea1619ccd2c78bd3fee24d6c5a43b97c3895
Sean Seefried <sean.seefried@gmail.com>
2011-02-21 17:08:21 +1100

    Removed redirection. Use routes.rb instead.

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :contact
 
+  map.root :controller => 'home'
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
